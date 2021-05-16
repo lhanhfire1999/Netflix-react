38b4de8c68b857023bf67d02216c94fb15a3448a
Anh Le <lhanhfire1999@gmail.com>
2021-05-16 12:55:56 +0700

    'add-Header-component-for-Homepage'

---
+++ src/components/header/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link as ReachRouterLink } from 'react-router-dom';
+import {
+  Container,
+  Group,
+  Background,
+  Dropdown,
+  Picture,
+  Link,
+  Search,
+  Profile,
+  FeatureCallOut,
+  SearchIcon,
+  SearchInput,
+  ButtonLink,
+  PlayButton,
+  Text,
+  Feature,
+  Logo,
+} from './styles/header';
+
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg ? (
+    <Background data-testid="header-bg" {...restProps}>
+      {children}
+    </Background>
+  ) : (
+    children
+  );
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (
+    <ReachRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReachRouterLink>
+  );
+};
+
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
