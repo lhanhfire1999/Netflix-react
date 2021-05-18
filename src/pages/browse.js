f9fef352e853c3a4351d8e0d80b30cee4a07ee63
Anh Le <lhanhfire1999@gmail.com>
2021-05-18 11:43:00 +0700

    'use-content-to-take-data'

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
-export default function Browse(){
-    return (<p>Hello from the browse!</p>)
+
+export default function Browse() {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(series);
+  console.log(films);
+
+  return <p>Hello from the browse</p>;
 }
